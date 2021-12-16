9bf1591186f3020a56b6447626594e44199fd88a
MrMohammadY <m.yazdani000@gmail.com>
2021-12-16 12:15:20 +0330

    Refactor(lib): rename base to model

--- lib/models/base.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-
-
-class BaseModel(models.Model):
-    created_time = models.DateField(auto_now_add=True, verbose_name=_('created time'))
-    modified_time = models.DateField(auto_now=True, verbose_name=_('created time'))
