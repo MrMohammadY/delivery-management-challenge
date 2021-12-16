86f39eeee4672d1b9c512c5734738f064b27f575
MrMohammadY <m.yazdani000@gmail.com>
2021-12-16 14:09:00 +0330

    Update(admin): update list display in model admin

--- salary/admin.py
+++ salary/admin.py
@@ -5,7 +5,7 @@ from salary.models import RewardDeduction, WeeklySalary, DailySalary
 
 @admin.register(RewardDeduction)
 class RewardDeductionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'courier', 'type', 'reason', 'description', 'price')
+    list_display = ('id', 'courier', 'type', 'reason', 'description', 'price', 'created_time', 'modified_time')
     list_filter = ('type', )
 
 
--- trip/admin.py
+++ trip/admin.py
@@ -5,4 +5,4 @@ from trip.models import Trip
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    list_display = ('id', 'courier', 'start_time', 'end_time', 'price')
+    list_display = ('id', 'courier', 'start_time', 'end_time', 'price', 'created_time', 'modified_time')
