5ad0174e4085f432ef0f3346281dad7b07a20ce2
MrMohammadY <m.yazdani000@gmail.com>
2021-12-17 16:04:16 +0330

    Build(celery): create and config celery

--- delivery/settings.py
+++ delivery/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_TIMEZONE = "Asia/Tehran"
+CELERY_TASK_TIME_LIMIT = 30 * 60
